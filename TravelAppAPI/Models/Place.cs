c8f7b104c97cbd7478e332bf60662bab7ded535e
quydt <quydt@BNC-SERVER>
2024-02-20 14:23:08 +0700

    Add project files.

---
+++ TravelAppAPI/Models/Place.cs
@@ -0,0 +1,14 @@
+﻿namespace TravelAppAPI.Model
+{
+    public class Place
+    {
+        public required int id { get; set; } = 0;
+        public string Name { get; set; } = String.Empty;
+        public string Description { get; set; } = String.Empty;
+        public string Image { get; set; } = String.Empty;
+        public string Location { get; set; } = String.Empty;
+        public string Rating { get; set; } = String.Empty;
+        public double Price { get; set; } = 0;
+
+    }
+}
