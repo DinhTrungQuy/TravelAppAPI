f1499d81a23fcdd8bb80c844a141888f7b0c48b1
quydt <quydt@BNC-SERVER>
2024-03-05 08:05:11 +0700

    feat: add booking services

---
+++ TravelAppAPI/Sevices/BookingServices.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using TravelAppAPI.Models;
+
+namespace TravelAppAPI.Sevices
+{
+    public class BookingServices
+    {
+        private readonly IMongoCollection<Booking> _bookings;
+        public BookingServices(IOptions<TravelAppDatabaseSettings> settings)
+        {
+            var client = new MongoClient(settings.Value.ConnectionString);
+            var database = client.GetDatabase(settings.Value.DatabaseName);
+            _bookings = database.GetCollection<Booking>(settings.Value.BookingsCollectionName);
+        }
+        public async Task<List<Booking>> GetAsync()
+        {
+            return await _bookings.Find(booking => true).ToListAsync();
+        }
+        public async Task<Booking> GetAsync(string id)
+        {
+            return await _bookings.Find<Booking>(booking => booking.Id == id).FirstOrDefaultAsync();
+        }
+        public async Task<Booking> CreateAsync(Booking booking)
+        {
+            await _bookings.InsertOneAsync(booking);
+            return booking;
+        }
+        public async Task UpdateAsync(string id, Booking bookingIn)
+        {
+            await _bookings.ReplaceOneAsync(booking => booking.Id == id, bookingIn);
+        }
+        public async Task RemoveAsync(Booking bookingIn)
+        {
+            await _bookings.DeleteOneAsync(booking => booking.Id == bookingIn.Id);
+        }
+
+    }
+}
