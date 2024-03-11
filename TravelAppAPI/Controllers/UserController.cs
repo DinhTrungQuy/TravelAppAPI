0121f1cbffc288181d001a06bf3364ccc10bf235
quydt <quydt@BNC-SERVER>
2024-03-11 11:55:17 +0700

    feat: add image upload api

--- TravelAppAPI/Controllers/UserController.cs
+++ TravelAppAPI/Controllers/UserController.cs
@@ -9,9 +9,12 @@ namespace TravelAppAPI.Controllers
     [Route("api/[controller]")]
     [ApiController]
     [Authorize]
-    public class UserController(UserServices userServices) : ControllerBase
+    public class UserController(UserServices userServices,FileServices fileServices, AuthServices authServices) : ControllerBase
     {
         private readonly UserServices _userServices = userServices;
+        private readonly FileServices _fileServices = fileServices;
+        private readonly AuthServices _authServices = authServices;
+
 
   
 
@@ -23,6 +26,22 @@ namespace TravelAppAPI.Controllers
             string userId = _userServices.DecodeJwtToken(request);
             return Ok(await _userServices.GetAsync(userId));
 
+        }
+        [HttpPost]
+        [Route("ImageUpload")]
+        public async Task<IActionResult> ImageUpload(IFormFile file)
+        {
+            var request = HttpContext.Request;
+            string userId = _userServices.DecodeJwtToken(request);
+            var filePath = await _fileServices.SaveUserFile(file, userId);
+            var user = await _userServices.GetAsync(userId);
+            if (user == null)
+            {
+                return BadRequest();
+            }
+            user.ImageUrl = "https://quydt.speak.vn/images/user" + userId + Path.GetExtension(filePath);
+            await _authServices.UpdateAsync(userId, user);
+            return Ok(user);
         }
     }
 }
